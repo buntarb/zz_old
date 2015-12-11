38aa11fce1fd02d901a3e4bf372872cea5a24bdc
buntarb <buntarb@webdev.(none)>
2015-12-11 18:42:14 +0200

    Add radio template.

---
+++ sources/zz/_template/radio.js
@@ -0,0 +1,25 @@
+// This file was automatically generated from radio.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace zz.template.ui.radio.
+ */
+
+goog.provide('zz.template.ui.radio');
+
+goog.require('soy');
+goog.require('soydata');
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {string}
+ * @suppress {checkTypes|uselessCode}
+ */
+zz.template.ui.radio.default = function(opt_data, opt_ignored) {
+  return '<label id="1" class="' + goog.getCssName('mdl-radio') + ' ' + goog.getCssName('mdl-js-radio') + ' ' + goog.getCssName('mdl-js-ripple-effect') + '"><input type="radio" class="' + goog.getCssName('mdl-radio__button') + ' name="options" value="1" checked"><span class="' + goog.getCssName('mdl-radio__label') + '">1st</span></label><br><br><label id="2" class="' + goog.getCssName('mdl-radio') + ' ' + goog.getCssName('mdl-js-radio') + ' ' + goog.getCssName('mdl-js-ripple-effect') + '"><input type="radio" class="' + goog.getCssName('mdl-radio__button') + '" name="options" value="2"><span class="' + goog.getCssName('mdl-radio__label') + '">2nd</span></label>';
+};
+if (goog.DEBUG) {
+  zz.template.ui.radio.default.soyTemplateName = 'zz.template.ui.radio.default';
+}
