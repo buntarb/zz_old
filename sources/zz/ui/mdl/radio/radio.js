ee5f11ae8138e88687077ebd3bb2630af7d88c8d
alex-popkov <popkov.aleksander@gmail.com>
2015-12-12 15:23:24 +0200

    some fix

--- sources/zz/ui/mdl/radio/radio.js
+++ sources/zz/ui/mdl/radio/radio.js
@@ -210,12 +210,13 @@ zz.ui.mdl.Radio.prototype.blurRadioListener_ = function( ){
  */
 zz.ui.mdl.Radio.prototype.changeRadioListener_ = function( ){
 
-	//var radios = document.getElementsByClassName(this.CssClasses_.JS_RADIO);
+	//var radios = goog.dom.getElementByClass(zz.ui.mdl.Radio.CSS.JS_RADIO);
+	//console.log(radios);
 	//for (var i = 0; i < radios.length; i++) {
-	//	var button = radios[i].querySelector('.' + this.CssClasses_.RADIO_BTN);
+	//	var button = radios[i].querySelectorAll('.' + zz.ui.mdl.Radio.CSS.RADIO_BTN);
 	//	// Different name == different group, so no point updating those.
 	//	if (button.getAttribute('name') === this.btnElement_.getAttribute('name')) {
-	//		radios[i]['MaterialRadio'].updateClasses_();
+	//		//this.getRenderer( ).updateClasses( this );
 	//	}
 	//}
 
