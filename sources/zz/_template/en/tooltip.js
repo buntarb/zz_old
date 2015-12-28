2eb4f03ded2fad58e9d7bff5705e674f1ef089af
buntarb <buntarb@webdev.(none)>
2015-12-28 20:44:03 +0200

    Commit to switch.

---
+++ sources/zz/_template/en/tooltip.js
@@ -0,0 +1,25 @@
+// This file was automatically generated from tooltip.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace zz.template.ui.tooltip.
+ */
+
+goog.provide('zz.template.ui.tooltip');
+
+goog.require('soy');
+goog.require('soydata');
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {string}
+ * @suppress {checkTypes|uselessCode}
+ */
+zz.template.ui.tooltip.default = function(opt_data, opt_ignored) {
+  return '<div id="1" class="' + goog.getCssName('icon') + ' ' + goog.getCssName('material-icons') + '">add</div><div id="tt-1" class="' + goog.getCssName('mdl-tooltip') + '" for="1">Follow me</div><div id="2" class="' + goog.getCssName('icon') + ' ' + goog.getCssName('material-icons') + '">print</div><div id="tt-2" class="' + goog.getCssName('mdl-tooltip') + ' ' + goog.getCssName('mdl-tooltip--large') + '" for="2">Print</div><div id="3" class="' + goog.getCssName('icon') + ' ' + goog.getCssName('material-icons') + '">cloud_upload</div><div id="tt-3" class="' + goog.getCssName('mdl-tooltip') + '" for="3">Upload <strong>file.zip</strong></div><div id="4" class="' + goog.getCssName('icon') + ' ' + goog.getCssName('material-icons') + '">share</div><div id="tt-4" class="' + goog.getCssName('mdl-tooltip') + '" for="4">Share your content<br>via social media</div>';
+};
+if (goog.DEBUG) {
+  zz.template.ui.tooltip.default.soyTemplateName = 'zz.template.ui.tooltip.default';
+}
