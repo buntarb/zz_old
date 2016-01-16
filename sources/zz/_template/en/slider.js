71138ee35d94fa9b91c7f8f12635eba430a7194c
buntarb <buntarb@webdev.(none)>
2016-01-16 22:06:22 +0200

    Menu open fixes

---
+++ sources/zz/_template/en/slider.js
@@ -0,0 +1,25 @@
+// This file was automatically generated from slider.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace zz.template.ui.slider.
+ */
+
+goog.provide('zz.template.ui.slider');
+
+goog.require('soy');
+goog.require('soydata');
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {string}
+ * @suppress {checkTypes|uselessCode}
+ */
+zz.template.ui.slider.default = function(opt_data, opt_ignored) {
+  return '<input id="1" class="' + goog.getCssName('mdl-slider') + ' ' + goog.getCssName('mdl-js-slider') + '" type="range" min="0" max="100" value="0" tabindex="0">';
+};
+if (goog.DEBUG) {
+  zz.template.ui.slider.default.soyTemplateName = 'zz.template.ui.slider.default';
+}
